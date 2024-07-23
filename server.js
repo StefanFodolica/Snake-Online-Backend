d91383bbf70cda47efd0d17d5ab74ce8ec0a9515
StefanFodolica <stefan.fodolica@gmail.com>
2024-07-23 14:44:15 +0300

    Update server.js

--- server.js
+++ server.js
@@ -15,10 +15,25 @@ app.use(cors({
 
 app.use(express.json());
 
+// Middleware to remove trailing slashes
+app.use((req, res, next) => {
+  if (req.path.substr(-1) === '/' && req.path.length > 1) {
+    const query = req.url.slice(req.path.length);
+    res.redirect(301, req.path.slice(0, -1) + query);
+  } else {
+    next();
+  }
+});
+
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', 'https://stefanfodolica.github.io');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log(`Received request for ${req.method} ${req.url}`);
   next();
 });
 
@@ -50,12 +65,6 @@ app.get('/api/leaderboard/:difficulty', (req, res) => {
   }
   res.json(leaderboards[difficulty]);
 });
-
-app.use((req, res, next) => {
-  console.log(`Received request for ${req.method} ${req.url}`);
-  next();
-});
-
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
